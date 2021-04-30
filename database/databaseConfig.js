ae843c8322472e899bdebca4ffa6474119131fe6
Cleytonpsf <83039268+Cleytonpsf@users.noreply.github.com>
2021-04-29 22:18:38 -0300

    Update databaseConfig.js

--- database/databaseConfig.js
+++ database/databaseConfig.js
@@ -3,8 +3,7 @@ const {Sequelize} = require('sequelize')
 const config = new Sequelize({
     host: "localhost",
     username: "root",
-    password: "18051300psf",
-    //password: "290520",
+    password: "290520",
     database: "projetoramo",
     dialect: "mysql",
     query: {raw: true}
