5007631ad5818ba9d91a9ba10caa246ab4e2ca24
Lucas Farolfi <lucasfarolfi.rj@gmail.com>
2021-05-12 03:45:33 -0300

    Criação de design responsivo na Navegação e no rodapé

---
+++ public/js/menu.js
@@ -0,0 +1,7 @@
+menu = () =>{
+    let menu = document.getElementById("menu")
+    let menuButton = document.getElementById("menu-button")
+
+    menu.classList.toggle("menu-active")
+    menuButton.classList.toggle("menu-button-active")
+}
