8336a06c3d9a8525a86b4ee2c9c724da2785fab8
Felipe dos Santos <felipe-junior@users.noreply.github.com>
2021-05-15 05:35:44 -0300

    Teste inserido para vizualização da pagina na reunião

--- routes/categorias.js
+++ routes/categorias.js
@@ -4,9 +4,20 @@ const slugify = require("slugify")
 
 //Rotas Get
 Router.get("/categorias", (req, res)=>{
-    res.render("categorias")
+    const arrayTemporario = [
+        {nome: "Esportes", qtd: 23},
+        {nome: "Noticias", qtd: 23},
+        {nome: "Saude", qtd: 23},
+        {nome: "Politica", qtd: 23},
+        {nome: "Mundo", qtd: 23},
+        {nome: "Variedades", qtd: 23},
+        {nome: "Lazer", qtd: 23},
+]
+    res.render("categorias", {categorias: arrayTemporario})
 })
 Router.get("/categorias/novo", (req, res)=>{
+
+    
     res.render("nova-categoria")
 })
 
