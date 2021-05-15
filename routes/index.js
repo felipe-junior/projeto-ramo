ec9bd1e5fc8b991f9265dbfe2253cb305bfd8fec
Lucas Farolfi <lucasfarolfi.rj@gmail.com>
2021-05-15 01:16:12 -0300

    Página da discussão criada

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const perguntar = require('./perguntar')
 const cadastrar = require("./cadastrar")
 const categorias = require('./categorias')
 const entrar = require('./entrar')
+const discussao = require('./discussao')
 
 rotas.use("/", home)
 rotas.use("/", cadastrar)
 rotas.use("/", perguntar)
 rotas.use("/", categorias)
 rotas.use("/", entrar)
+rotas.use("/", discussao)
 
 module.exports = rotas
