257a9da3ba9d1dbd89480fb421911ac8e29cca5e
Lucas Farolfi <lucasfarolfi.rj@gmail.com>
2021-06-14 03:21:32 -0300

    Criação das rotas de Esqueceu sua senha

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,18 @@
 const Router = require("express").Router()
 const User = require("../database/models/login")
 const bcrypt = require("bcryptjs")
+const login = require("../database/models/login")
 //const Categories = require("../database/models/")
 
 Router.get("/entrar", (req, res)=>{
-    let session
     if(req.session.user != undefined){
-        session = true
+        res.redirect("/")
     }
     else{
-        session = false
+        let session = false
+        res.render("login",  {session})
     }
-    res.render("login",  {session})
+    
 })
 Router.post("/entrar", async (req, res)=>{
     
@@ -26,13 +27,49 @@ Router.post("/entrar", async (req, res)=>{
                 res.redirect(req.session.returnTo || "/")
             }
         }
+        else{
+            res.redirect("/entrar")
+        }
     })   
 })
 
 Router.get("/sair", (req, res)=>{
     req.session.user = undefined
 
-    res.redirect("/")
+    res.redirect("/entrar")
 })
 
+//Esqueceu sua senha
+Router.get("/esqueceu-sua-senha", async (req, res)=>{
+    if(req.session.user != undefined){
+        res.redirect("/")
+    }
+    else{
+        let session = false
+        let registers = await login.findAll()
+        res.render("forgottenPassword", {session, registers}) 
+    }
+    
+})
+
+Router.post("/alterar-senha", (req, res)=>{
+    let {email, password} = req.body
+
+    login.findOne({where:{email}}).then(user=>{
+        if(user != undefined){
+            let salt = bcrypt.genSaltSync(10)
+            let hash = bcrypt.hashSync(password, salt)
+
+            user.update({
+                password: hash
+            }).then(()=>{
+                req.session.user = user.email
+                res.redirect("/")
+            })
+        }
+        else{
+            res.redirect("/")
+        }
+    })
+})
 module.exports = Router
