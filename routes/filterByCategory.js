c327722933faaf55401d0dea4c2733ed213963a1
Lucas Farolfi <lucasfarolfi.rj@gmail.com>
2021-06-15 01:02:24 -0300

    Correção de problema que não amostra o usuário que enviou a pergunta

--- routes/filterByCategory.js
+++ routes/filterByCategory.js
@@ -2,23 +2,31 @@ const Category = require("../database/models/category")
 const Question = require("../database/models/question")
 const formatDate = require("../public/js/formatDate")
 const Router = require("express").Router()
-
+const User = require("../database/models/login")
 
 Router.get("/categoria/:slug", async (req, res)=>{
     let session = req.session.user != undefined ? true : false
     const slug = req.params.slug
     
+    let category = await Category.findOne({where:{slug}})
+
+    if(category!=undefined){
+        let discussions = await Question.findAll({
+            where:{categoryId: category.id}, include: [{model:Category}, {model: User}], order:[['id','DESC']]
+        })
+        res.render("filterByCategory", {discussions, formatDate, category, session})
+    } else{
+        res.redirect("/")
+    }
+    
+
     Category.findOne({
         where: {
             slug: slug
         }, 
         include: [{model: Question}]
     }).then(category =>{
-        if(category!=undefined){
-            res.render("filterByCategory", {discussions: category.questions, formatDate, category, session})
-        } else{
-            res.redirect("/")
-        }
+        
     })        
 })
 module.exports = Router
