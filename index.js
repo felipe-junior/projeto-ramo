264989eea078580868da3f9ca29b68fb8eeabf25
Cleytonpsf <cleytonpsf@gmail.com>
2021-07-15 15:21:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const session = require("express-session")
 const toMili = require("./public/js/toMili")
 const init_cat = require("./initialCategories")
 const relations = require("./database/relations")
+const flash = require("express-flash")
+const cookieParser = require("cookie-parser")
 const port = 8080
 
 database.authenticate()
@@ -19,14 +21,18 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+app.use(cookieParser("eg1e6h1dj6168v6q8wf68h3c"))
 app.use(session({
-    secret: "ashdsayu8gdyu8as",
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
     cookie: {
         maxAge: toMili(60*60) // 1hr
     }
 }))
+app.use(flash())
 
-app.use("/", require("./routes/index")) // A rota index é responsavel por importar todas as rotas
+app.use("/", require("./routes/routes")) // A rota index é responsavel por importar todas as rotas
 
 app.listen(port, ()=>{
     console.log("O servidor está rodando...")
